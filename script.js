6e0f425d6da2de22f8aebb82bde13c471486be26
wilSage <wappiah308@gmail.com>
2025-10-06 22:15:39 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
+const menuOpen = document.querySelector('.menu-icon');
+const menuPopup = document.querySelector('.menu-popup');
+const menuIconClose = document.querySelector('.menu-icon-close');
+const menuLogIn = document.querySelector('.menu-login');
 
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
@@ -19,3 +23,18 @@ btnPopup.addEventListener('click', () => {
 iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-btnpopup');
 })
+
+
+menuOpen.addEventListener('click', () => {
+    menuPopup.classList.add('active-menu');
+})
+
+menuIconClose.addEventListener('click', () => {
+    menuPopup.classList.remove('active-menu');
+})
+
+menuLogIn.addEventListener('click', () => {
+    wrapper.classList.add('active-btnpopup');
+})
+
+
